42b0f896a5e7a5276d3a67c48ecabda2279e69e7
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2025-11-14 19:04:50 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
