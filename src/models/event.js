c4b6598abdbfffe14867ce7c92822d6942bbdfea
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2025-11-20 00:56:04 +0200

    fixed bug in event model

--- src/models/event.js
+++ src/models/event.js
@@ -5,41 +5,22 @@ import mongoose from 'mongoose';
  * Event schema.
  */
 const eventSchema = new mongoose.Schema({
-  eventId: {
-    type: Number,
-    required: true,
-    unique: true
-  },
+  // no eventId
   creatorId: {
     type: String,
-    required: true // id of Account
+    required: true
   },
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
+  title: { type: String, required: true, trim: true },
   description: String,
-  category: {
-    type: String,
-    required: true
-  },
-  dateTime: {
-    type: Date,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
+  category: { type: String, required: true },
+  dateTime: { type: Date, required: true },
+  location: { type: String, required: true },
   latitude: Number,
   longitude: Number,
   imageUrl: String,
-  isCancelled: {
-    type: Boolean,
-    default: false
-  }
+  isCancelled: { type: Boolean, default: false }
 });
+
 const Event =
   mongoose.models.Event || mongoose.model('Event', eventSchema);
 
